a7d525096711f487c87489fd2517b3af8588f41b
willpan <panpengbo@storswift.com>
2024-08-13 10:36:21 +0800

    chore: Add .gitignore

--- backend/powervoting/main.go
+++ backend/powervoting/main.go
@@ -19,7 +19,7 @@ func InitMysql() *gorm.DB {
 	var err error
 	engine, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:                       fmt.Sprintf("%s:%s@tcp(%s)/powervoting?charset=utf8&parseTime=True&loc=Local", "root", "Csystem32...", "localhost"),
-		DefaultStringSize:         256,   // string 类型字段的默认长度
+		DefaultStringSize:         256,   // string default length
 		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
 		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
 		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
